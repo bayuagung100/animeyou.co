88f782014a328aa571f838b49bf57e02b9cc5b49
bayuagung100 <bayuagung100@gmail.com>
2020-01-14 18:36:54 +0700

    tambah iklan adskeeper

---
+++ ads/native.php
@@ -0,0 +1,9 @@
+<style>#M518316ScriptRootC785417 {min-height: 300px;}</style>
+<!-- Composite Start -->
+    <div id="M518316ScriptRootC785417"> 
+        <div id="M518316PreloadC785417"> Loading... 
+        </div> 
+        </div>
+        <script src="//jsc.adskeeper.co.uk/a/n/animeyou.co.785417.js" async>
+        </script>
+<!-- Composite End -->
